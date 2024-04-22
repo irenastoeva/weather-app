c830a24415c7779073df5fe392c50d9071f282d4
Irena Stoeva <r.stoeva21@gmail.com>
2024-04-22 16:50:04 +0300

    ts2339 error fix

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,19 +1,20 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { environment } from '../../environments/environment';
-
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
+  api_base_url = 'http://api.weatherapi.com/v1';
+  api_key ='caa9b3bb7ea946b1ba5122924241904';
+
   constructor(private http: HttpClient) { }
 
   getCurrentWeather(city: string) {
-    return this.http.get(`${environment.api_base_url}/current.json`, {
+    return this.http.get(`${this.api_base_url}/current.json`, {
       params: new HttpParams()
-      .set('key', environment.api_key)
+      .set('key', this.api_key)
       .set('q', city)
     })
   }
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    api_base_url: 'http://api.weatherapi.com/v1',
-    api_key: 'caa9b3bb7ea946b1ba5122924241904'
-};
